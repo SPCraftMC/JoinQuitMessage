82bdd70ee1f5922c6b714e15f4749c6531cb84ae
XiaMoHuaHuo_CN <2549619802@qq.com>
2023-08-08 11:01:42 +0800

    Update Main.java

--- src/main/java/moe/xmcn/spcraft/joinquitmessage/Main.java
+++ src/main/java/moe/xmcn/spcraft/joinquitmessage/Main.java
@@ -6,18 +6,12 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onLoad() {
-        System.out.println("Loading SPCraft Plugin-0 Name: JoinQuitMessage");
         saveDefaultConfig();
     }
 
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new Event(), this);
-    }
-
-    @Override
-    public void onDisable() {
-        System.out.println("Disabling SPCraft Plugin-0 Name: JoinQuitMessage");
     }
 
 }
